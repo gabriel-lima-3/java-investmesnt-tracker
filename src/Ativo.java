24e236c87c44f852d7736307d97b7b420448f47e
Gabriel Lima <Gabriellimma00@gmail.com>
2025-11-18 14:42:56 -0300

    feat: adiciona caracteristicas na classe Ativo

---
+++ src/Ativo.java
@@ -0,0 +1,14 @@
+public class Ativo {
+
+    private String codigo;
+    private String nome;
+    private String tipo;
+    private String risco;
+    private double rentabilidade;
+    private double valorAtual;
+    private double variacaoAcumulada;
+    LinkedList<Double> historicoRentavel;
+
+
+
+}
